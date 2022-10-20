f7165f05b7035aea5371406a7c18d3b2ad99854d
Miriã Alves <116230299+mirian0504@users.noreply.github.com>
2022-10-20 01:16:59 +0000

    exibir varias variáveis

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ idade = 22
 
 print("Seu nome é " +nome )
 
+#qdo quiser exibir a frase
+
+print("Minha idade é " +str(idade) )
+
+#formatar inteiro
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+print("Minha idade é", +idade)
+
+
+print("meu nome é {} e tenho {}".format(nome, idade))
+
+
